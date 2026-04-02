52545330df1672034de2787d18f453a281092b27
Nguyen Hoang Kha <imsherlockholmes1403@gmail.com>
2026-04-02 09:30:43 +0700

    update database_url

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
     define: {
@@ -28,11 +24,7 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
     define: {
